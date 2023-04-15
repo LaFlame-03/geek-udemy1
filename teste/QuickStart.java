dc9934935c60f11cfe7ffecc9025322024f6e06a
LaFlame-03 <derickfelipe2003@gmail.com>
2023-04-15 14:19:32 -0300

    input java

---
+++ teste/QuickStart.java
@@ -0,0 +1,18 @@
+package teste;
+import java.io.InputStreamReader;
+import java.util.Date;
+import java.util.jar.*;
+public class QuickStart {
+    
+        public static void main(String[] args){
+            System.out.println("Hello World");
+            
+            String i = "";
+        
+            System.out.println("Ponto de entrada - Chamada da Tela Principal!");
+            i = System.console().readLine();
+            System.out.println(i);
+
+        }        
+        
+}

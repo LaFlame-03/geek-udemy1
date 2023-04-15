1e43e5e89f4f41d5c788643db487f0a279006ea2
LaFlame-03 <derickfelipe2003@gmail.com>
2023-04-15 15:49:47 -0300

    testefgwf

---
+++ teste/QuickStart.java
@@ -0,0 +1,18 @@
+package teste;
+import java.io.InputStreamReader;
+import java.util.Date;
+import java.util.jar.*;
+public class QuickStart {
+    
+        public static void main(String[] args){
+            System.out.println("Hello World");
+            
+            String i = "";
+        
+            System.out.println("digite seu nome");
+            i = System.console().readLine();
+            System.out.println(i);
+
+        }        
+        
+}
